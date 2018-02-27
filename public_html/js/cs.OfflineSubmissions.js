7ec4afdd80132d687961077b64be951182436d2b
Dan Wong <wongd@devlan.bits>
2018-02-27 00:39:33 -0500

    Updated to stratus forms 1.5. Moved javascript to OfflineSubmissions

--- public_html/js/cs.OfflineSubmissions.js
+++ public_html/js/cs.OfflineSubmissions.js
@@ -20,13 +20,13 @@ cs.OfflineSubmissions.StratusFormsSaveFormOffline = function (listName, formID,
         StratusFormsValuePairs, saveCompleteFunc, StratusFormsChildListData) {
     console.log("saving offline");
     console.log(StratusFormsValuePairs);
-    
+
     $a = $('<a>');
-    $a.attr('href', 'data:text/plain;charset=utf-8,' + 
+    $a.attr('href', 'data:text/plain;charset=utf-8,' +
             encodeURIComponent(JSON.stringify(StratusFormsValuePairs)));
     $a.attr('download', listName + '_' + formID + '.txt');
     $a[0].click();
-    
+
     if (saveCompleteFunc) {
         // What to pass for the new id?
         var newId = formID;
@@ -38,10 +38,10 @@ cs.OfflineSubmissions.StratusFormsSaveFormOffline = function (listName, formID,
  * Override the save function if offline.
  * @returns {undefined}
  */
-cs.OfflineSubmissions.configureOffline = function() {
+cs.OfflineSubmissions.configureOffline = function () {
     $.fn.StratusFormsSaveForm = (function () {
         var original = $.fn.StratusFormsSaveForm;
-        
+
         console.log("offline? " + cs.OfflineSubmissions.offline);
         if (cs.OfflineSubmissions.offline) {
             // Offline.  Save as text file.
@@ -52,7 +52,33 @@ cs.OfflineSubmissions.configureOffline = function() {
     })();
 };
 
-$(function() {
+
+cs.OfflineSubmissions.submitForm = function() {
+    //When the form is submitted store it to the specified list
+    //also pasas in the x and y offset of error messages for elements
+    //this allows you to change their location in reference to the form field
+    $("#SFForm").StratusFormsSubmit({
+        listName: "MyList",
+        completefunc: function (id) {
+            // Don't reload when offline.
+            if (!cs.OfflineSubmissions.offline) {
+                alert("Save was successful. ID = " + id);
+                window.location = window.location.pathname + "?formID=" + id;
+            }
+        }
+    });
+};
+
+
+
+$(function () {
+    //Initialization function. Tells StratusForms which Query String Variable
+    //has the ID of the form, and the name of the list to read data from
+    $("#SFForm").StratusFormsInitialize({
+        queryStringVar: "formID",
+        listName: "MyList"
+    });
+
     // Override on document ready to allow html page to set the offline flag.
     cs.OfflineSubmissions.configureOffline();
 });

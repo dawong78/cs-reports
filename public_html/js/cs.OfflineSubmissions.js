8cf3f6c1d3b2cda4f21e9d287560bee56d7867dc
Dan Wong <wongd@devlan.bits>
2018-03-09 00:31:52 -0500

    add ability to edit

--- public_html/js/cs.OfflineSubmissions.js
+++ public_html/js/cs.OfflineSubmissions.js
@@ -22,10 +22,12 @@ cs.OfflineSubmissions.StratusFormsSaveFormOffline = function (listName, formID,
     console.log(StratusFormsValuePairs);
 
     $a = $('<a>');
-    $a.attr('href', 'data:text/plain;charset=utf-8,' +
-            encodeURIComponent(JSON.stringify(StratusFormsValuePairs)));
+    $a.css({'visible': false});
+    $a.attr('href', 'data:text/plain;charset=utf-8,' + StratusFormsValuePairs[0][1]);
     $a.attr('download', listName + '_' + formID + '.txt');
+    $('body').append($a);
     $a[0].click();
+    $a.remove();
 
     if (saveCompleteFunc) {
         // What to pass for the new id?
@@ -34,11 +36,35 @@ cs.OfflineSubmissions.StratusFormsSaveFormOffline = function (listName, formID,
     }
 };
 
+/**
+ * Override the load behavior.  Instead of grabbing data from sharepoint,
+ * grab it from a file the user selects.
+ * @param {type} form
+ * @param {type} id
+ * @param {type} listName
+ * @param {type} StratusFormsDataField
+ * @param {type} listFieldsArray
+ * @returns {Promise}
+ */
+cs.OfflineSubmissions.StratusFormsLoadFormFields = function (form, id, listName, 
+        StratusFormsDataField, listFieldsArray) {
+    var $defer = $.Deferred();
+    cs.OfflineSubmissions.readFiles()
+            .done(function(json) {
+                $defer.resolve(json, '', null, listFieldsArray);
+            });
+    return $defer;
+};
+
+/**
+ * Configure for offline or online.
+ * If get() fails, then we are offline.
+ * @returns
+ */
 cs.OfflineSubmissions.configure = function () {
     return $.get("/")
             .done(function () {
                 cs.OfflineSubmissions.offline = false;
-                cs.OfflineSubmissions.configureOffline();
             })
             .fail(function () {
                 cs.OfflineSubmissions.offline = true;
@@ -47,53 +73,76 @@ cs.OfflineSubmissions.configure = function () {
 };
 
 /**
- * Override the save function if offline.
+ * Override the sharepoint functions if offline.
  * @returns {undefined}
  */
 cs.OfflineSubmissions.configureOffline = function () {
-    $.fn.StratusFormsSaveForm = (function () {
-        var original = $.fn.StratusFormsSaveForm;
+    // Override the save function
+    $.fn.StratusFormsSaveForm = cs.OfflineSubmissions.StratusFormsSaveFormOffline;
+    
+    // Override the load function
+    $.fn.StratusFormsLoadFormFields = cs.OfflineSubmissions.StratusFormsLoadFormFields;
+    
+    cs.OfflineSubmissions.addOpenButton();
+};
 
-        console.log("offline? " + cs.OfflineSubmissions.offline);
-        if (cs.OfflineSubmissions.offline) {
-            // Offline.  Save as text file.
-            return cs.OfflineSubmissions.StratusFormsSaveFormOffline;
-        } else {
-            return original;
-        }
-    })();
+cs.OfflineSubmissions.addOpenButton = function() {
+    // Add an open button
+    
+    var $div = $('<div>');
+    var $fileInput = $('<input id="cs-file" type="file">');
+    $fileInput.css({'display': 'none'});
+    $fileInput.change(function() {
+        $('#cs-file-name').val($('#cs-file').val());
+        cs.OfflineSubmissions.initializeForm();
+    });
+    $div.append($fileInput);
+    
+    var $textInput = $('<input id="cs-file-name" readonly>');
+    $div.append($textInput);
+    
+    var $openButton = $('<button>Open</button>');
+    $openButton.click(function() {
+        $('#cs-file')[0].click();
+    });
+    $div.append($openButton);
+    // Add to top of page
+    $('#SFForm').before($div);
 
 };
 
-cs.OfflineSubmissions.selectFile = function() {
-    var readFiles = function (evt) {
-        var files = evt.target.files; // FileList object
-        var f = files[0];
-        var reader = new FileReader();
-        reader.onload = (function (event) {
-            setContents(event.target.result);
-        });
-        // Read in the image file as a data URL.
-        reader.readAsText(f);
-    };
-    
-    var setContents = function(data) {
-        $('#offlineSubmissions-content').html(data);
-    };
+/**
+ * When loading a file, need to call StratusFormsInitialize.
+ * @returns {undefined}
+ */
+cs.OfflineSubmissions.initializeForm = function() {
+    $("#SFForm").StratusFormsInitialize({
+        'listID': 1
+    });
+};
 
-    var $fileInput = $('<input>');
-    $fileInput.attr('type', 'file')
-    $fileInput.val('content.html')
-    $fileInput.click(readFiles);
-    $fileInput[0].click();
+/**
+ * Select a file, load its contents, pass the contents to the promise.
+ * @returns {Promise}
+ */
+cs.OfflineSubmissions.readFiles = function() {
+    var files = $('#cs-file')[0].files;
+    var $d = new $.Deferred();
+    var f = files[0];
+    var reader = new FileReader();
+    reader.onload = (function (event) {
+        $d.resolve(event.target.result);
+    });
+    // Read in the image file as a data URL.
+    reader.readAsText(f);
+    return $d;
+};
 
-}
-
-
+/**
+ * Call to save the form.
+ */
 cs.OfflineSubmissions.submitForm = function () {
     //When the form is submitted store it to the specified list
-    //also pasas in the x and y offset of error messages for elements
-    //this allows you to change their location in reference to the form field
     $("#SFForm").StratusFormsSubmit({
         listName: "MyList",
         completefunc: function (id) {
